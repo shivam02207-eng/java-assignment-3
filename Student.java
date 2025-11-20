b62ff200de4c055ea6161f2afb9a164f1fe2592f
shivam02207-eng <shivam02207@gmail.com>
2025-11-20 10:30:49 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,41 @@
+public class Student {
+    private int rollNumber;
+    private String studentName;
+    private int[] marks = new int[3];
+
+    public Student(int rollNumber, String studentName, int[] marks) {
+        this.rollNumber = rollNumber;
+        this.studentName = studentName;
+        this.marks = marks;
+    }
+
+    public void validateMarks() throws InvalidMarksException {
+        for (int i = 0; i < marks.length; i++) {
+            if (marks[i] < 0 || marks[i] > 100) {
+                throw new InvalidMarksException("Invalid marks for subject " + (i + 1) + ": " + marks[i]);
+            }
+        }
+    }
+
+    public double calculateAverage() {
+        int sum = 0;
+        for (int m : marks) sum += m;
+        return sum / 3.0;
+    }
+
+    public void displayResult() {
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Student Name: " + studentName);
+        System.out.print("Marks: ");
+        for (int m : marks) System.out.print(m + " ");
+        System.out.println();
+        double avg = calculateAverage();
+        System.out.println("Average: " + avg);
+        if (avg >= 40) System.out.println("Result: Pass");
+        else System.out.println("Result: Fail");
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+}
